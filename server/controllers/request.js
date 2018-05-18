3f971e9fd0099f3574944426b57ce69b22de0350
Oluwafayokemi <fayoaright@gmail.com>
2018-05-18 02:21:09 +0100

    [ch-#157684595]set up controller router and validator

---
+++ server/controllers/request.js
@@ -0,0 +1,25 @@
+import db from './db/db';
+
+class Request {
+    getAll(req, res) {
+        
+    }
+
+    getOne(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    remove(req, res) {
+
+    }
+}
+const request = new Request();
+export default request;
