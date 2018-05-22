104d8da4c48565b8f0de08ba394e7daeecad2493
Oluwafayokemi <fayoaright@gmail.com>
2018-05-22 16:21:19 +0100

    [ch-#157734616]finish set up datase and test to get a request

---
+++ server/db/index.js
@@ -0,0 +1,15 @@
+import pg from 'pg';
+
+const config = {
+  user: 'postgres',
+  database: 'maintenance-tracker',
+  password: '',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+}
+
+const db = new pg.Pool(config);
+
+export default db
