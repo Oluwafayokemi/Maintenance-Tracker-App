36e3838e2f3da3edb663a595d3c1862b10abf863
Adeyina Fayoke Comfort <33626790+Oluwafayokemi@users.noreply.github.com>
2018-05-23 12:45:49 +0100

    Revert "[ch-#157734616]finish set up database and test to get a request"

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -1,5 +1,5 @@
 /* eslint-disable class-methods-use-this */
-import db from '../db/index';
+import db from '../db/db';
 
 /**
  * @export
@@ -7,36 +7,127 @@ import db from '../db/index';
  */
 
 class Request {
-    /**
-       * Get Multiple request record
-       *
-       * @param {object} req - HTTP Request
-       * @param {object} res - HTTP Response
-       * @returns {object} Class instance
-       * @memberof Requests
-       */
-    /*get All Requests */
-
+/**
+   * Get Multiple request record
+   *
+   * @param {object} req - HTTP Request
+   * @param {object} res - HTTP Response
+   * @returns {object} Class instance
+   * @memberof Meals
+   */
+  /*get All Meals */
     getAll(req, res) {
-        db.on('error', (err, client) => {
-            console.error('Unexpected error on idle client', err)
-            process.exit(-1)
+        res.status(201).json({
+            success: 'true',
+            message: 'successfully retrieved all requests',
+            requests: db.requests
         })
-        db.connect()
-            .then(client => {
-                client.query('SELECT * from requests')            
-                    .then(requests => res.status(200).json({
-                        success: 'true',
-                        message: 'all requests retrieved successfully',
-                        requests: requests.rows
-                    }))
-                    .catch(error => res.status(400).json({
-                        success: 'false',
-                        message: 'could not retrieve requests',
-                    }))
+    }
+/**
+     * Get a single request record
+     *
+     * @param {object} req - HTTP Request
+     * @param {object} res - HTTP Response
+     * @returns {object} Class instance
+     * @memberof request
+     */
+    getOne(req, res) {
+        const id = parseInt(req.params.id, 10);
+        if ( id > db.requests.length ) {
+            res.status(404).json({
+                success: 'false',
+                message: 'request not found'
+            })
+        }
+        db.requests.find(request => {
+            if (request.id === id) {
+                return res.status(201).json({
+                    success: 'true',
+                    message: 'request sucessfuly retrieved',
+                    request: request
+                })
+            }
+           
+        });
+    }
+
+  /* create meals */
+    create(req, res) {
+        const id = parseInt(req.params.id, 10);
+        const { name, option, department, description } = req.body
+        db.requests.push({
+            id: db.requests.length + 1,
+            name,
+            option,
+            department,
+            description,
+            date: new Date()
+        })
+        res.status(201).json({
+            success: 'true',
+            message: 'successfully created new request',
+            newRequest: db.requests[db.requests.length - 1]
+        });
+    }
+
+  /* update meals */
+    update(req, res) {
+        const id = parseInt(req.params.id, 10);
+        let requestFound;
+        let requestIndex;
+        db.requests.find((request, index) => {
+            if (request.id === id) {
+                requestFound = request;
+                requestIndex = index;
+            }
+
+        });
+        if (!requestFound) {
+            return res.status(404).send({
+                success: 'false',
+                message: 'request not found',
             });
+        }
+
+        const updatedRequest = {
+            id: requestFound.id,
+            name: req.body.name || requestFound.name,
+            description: req.body.description || requestFound.description,
+            department: req.body.department || requestFound.department,
+            option: req.body.option || requestFound.option,
+        };
+        db.requests.splice(requestIndex, 1, updatedRequest);
+
+        return res.status(200).json({
+            success: 'true',
+            message: 'request updated successfully',
+            updatedRequest,
+        });
+        if (!requestFound) {
+            res.status(404).json({
+                success: 'false',
+                message: 'request not found',
+            });
+        }
+    }
+
+  /* delete a request record */
+    remove(req, res) {
+        const id = parseInt(req.params.id, 10);
+        db.requests.find((request, index) => {
+            if (request.id === id) {
+                db.requests.splice(index, 1);
+                return res.status(202).json({
+                    success: 'true',
+                    message: 'request sucesfully deleted',
+                });
+            }
+        });
+        return res.status(404).json({
+            success: 'false',
+            message: 'request not found'
+        })
     }
 }
-
 const request = new Request();
 export default request;
