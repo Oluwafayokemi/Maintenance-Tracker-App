2b65c25d4d99b6a9f037c8ceb3fcf549c84e013b
Oluwafayokemi <fayoaright@gmail.com>
2018-06-20 14:04:41 +0100

    fix test bg

--- server/models/db.js
+++ server/models/db.js
@@ -16,13 +16,13 @@ const salt = bcrypt.genSaltSync(Math.floor(Math.random() * 5));
 const encryptedPassword = bcrypt.hashSync(password, salt);
 
 const creatUserTable = ';DROP TABLE IF EXISTS users CASCADE; CREATE TABLE users(userId SERIAL PRIMARY KEY NOT NULL, firstName VARCHAR(100) NOT NULL, lastName VARCHAR(100) NOT NULL, isAdmin BOOLEAN NOT NULL DEFAULT FALSE, email VARCHAR(100) UNIQUE NOT NULL, password VARCHAR(400) NOT NULL, department VARCHAR(100), joined TIMESTAMP DEFAULT NOW())';
-const createRequestTable = ';DROP TABLE IF EXISTS requests CASCADE; CREATE TABLE requests(requestId SERIAL PRIMARY KEY NOT NULL, firstName VARCHAR(100) NOT NULL, lastName VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL, department VARCHAR(100) NOT NULL, userId SERIAL NOT NULL, equipment VARCHAR(100) NOT NULL,  description TEXT NOT NULL, status VARCHAR(100), date TIMESTAMP DEFAULT NOW())';
+const createRequestTable = ';DROP TABLE IF EXISTS requests CASCADE; CREATE TABLE requests(requestId SERIAL PRIMARY KEY NOT NULL, userId SERIAL NOT NULL, equipment VARCHAR(100) NOT NULL,  description TEXT NOT NULL, status VARCHAR(100), date TIMESTAMP DEFAULT NOW())';
 const insertUserTable = `;INSERT INTO users(firstName, lastName, isAdmin, email, password, department) VALUES('fayokemi', 'adeyina', TRUE, 'fayoaright@gmail.com', '${encryptedPassword}', 'Water Management')`;
 const insertUserKayTable = `;INSERT INTO users(firstName, lastName, isAdmin, email, password, department) VALUES('kayode', 'adeyina', TRUE, 'kayode@gmail.com', '${encryptedPassword}', 'Water Management')`;
 const insertUser2Table = `;INSERT INTO users(firstName, lastName, email, password, department) VALUES('omotola', 'adeyina', 'omotola@gmail.com', '${encryptedPassword}', 'Water Management')`;
-const insertRequestTable = ';INSERT INTO requests(userId, firstName, lastName, email, department, equipment, description, status) VALUES(1, \'fayokemi\', \'adeyina\', \'fayoaright@gmail.com\', \'organizing\', \'generator\', \'Generator goes off always\', \'pending\')';
-const insertRequestKayTable = ';INSERT INTO requests(userId, firstName, lastName, email, department, equipment, description, status) VALUES(1, \'fayokemi\', \'adeyina\', \'fayoaright@gmail.com\', \'organizing\', \'generator\', \'Generator goes off always\', \'approve\')';
-const insertRequest2Table = ';INSERT INTO requests(userId, firstName, lastName, email, department, equipment, description, status) VALUES(2, \'omotola\', \'adeyina\', \'omotola@gmail.com\', \'medical\', \'Air Condition\', \'air condition blows hot air\', \'resolved\')';
+const insertRequestTable = ';INSERT INTO requests(userId, equipment, description, status) VALUES(1, \'generator\', \'Generator goes off always\', \'pending\')';
+const insertRequestKayTable = ';INSERT INTO requests(userId, equipment, description, status) VALUES(1, \'generator\', \'Generator goes off always\', \'approve\')';
+const insertRequest2Table = ';INSERT INTO requests(userId, equipment, description, status) VALUES(2, \'Air Condition\', \'air condition blows hot air\', \'resolved\')';
 const Query = `${creatUserTable} ${createRequestTable} ${insertUserTable} ${insertUserKayTable} ${insertUser2Table} ${insertRequestTable} ${insertRequestKayTable} ${insertRequest2Table}`;
 db.query(Query, (err, res) => {
   console.log(err, res);
